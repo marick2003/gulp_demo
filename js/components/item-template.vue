1a98e751ce351d564aa2571c1f411a1b98fca71c
marick <mmaarriicckk@gmail.com>
2019-11-05 18:23:02 +0800

    add

---
+++ js/components/item-template.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+
+
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="scss">
+    body {
+    font-family: Menlo, Consolas, monospace;
+    color: #444;
+    }
+    .item {
+    cursor: pointer;
+    }
+    .bold {
+    font-weight: bold;
+    }
+    ul {
+    padding-left: 1em;
+    line-height: 1.5em;
+    list-style-type: dot;
+    }
+</style>
